e396b8d52fee9d7d1e27b9d5b81ae39635888138
frozar <fabien.rozar@gmail.com>
2022-07-21 09:08:02 +0400

    3 - Routes

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,24 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(["prefix" => "admin", "middleware" => "auth"], function () {
+    Route::get("salut", function () {
+        return "Salut les gens";
+    });
+});
+
+Route::get('salut', function () {
+    return "Salut les gens";
+});
+
+Route::get('salut/{slug}-{id}', ["as" => "salut", function ($slug, $id) {
+    // return "Slug : $slug, ID: $id";
+    // return "Lien : /salut/$slug-$id";
+    return "Lien : " . route("salut", ["slug" => $slug, "id" => $id]);
+}])->where('slug', '[a-z0-9\-]+')->where('id', '[0-9]+');
